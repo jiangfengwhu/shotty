37c5996c39a5494c34cbb495b0f0af7bd1350b47
fengjiang <jiangfeng11@kuaishou.com>
2024-09-18 15:05:54 +0800

    Initial Commit

---
+++ shotty/shottyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shottyApp.swift
+//  shotty
+//
+//  Created by Feng Jiang on 2024/9/18.
+//
+
+import SwiftUI
+
+@main
+struct shottyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
